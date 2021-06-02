5c695fc5ad584e2765a32c9cc424c43f8502c9ae
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-02 14:18:39 -0500

    React renderer test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,17 @@
 const sum = require('../sum');
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../client/src/index'
 
-test('adds 1 + 3 to equal 4', () => {
-  expect(sum(1,3)).toBe(4);
+describe('Testing Jest to make sure it works', () => {
+  test('adds 1 + 3 to equal 4', () => {
+    expect(sum(1,3)).toBe(4);
+  })
+
+  test('Renders Hello World', () => {
+    const app = renderer.create(<App />)
+    let tree = app.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 })
+
