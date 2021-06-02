3e37112bb614beda099f5f339c28c8cd9b217650
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-02 14:36:42 -0500

    Set up enzyme

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,17 +1,24 @@
 const sum = require('../sum');
 import React from 'react';
-import renderer from 'react-test-renderer';
-import App from '../client/src/index'
+
+import App from '../client/src/index';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+import {shallow} from 'enzyme';
+
 
 describe('Testing Jest to make sure it works', () => {
-  test('adds 1 + 3 to equal 4', () => {
+  it('adds 1 + 3 to equal 4', () => {
     expect(sum(1,3)).toBe(4);
   })
 
-  test('Renders Hello World', () => {
-    const app = renderer.create(<App />)
-    let tree = app.toJSON();
-    expect(tree).toMatchSnapshot();
+  it('Renders Hello World', () => {
+    const app = shallow(<App />)
+
+    expect(app.find('#test').exists()).toBe(true);
   })
 })
 
