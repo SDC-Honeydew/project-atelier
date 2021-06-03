cd9b9da5ef13ccdad7b50da3d248d7ddd6e49389
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-02 21:52:58 -0500

    Set up eslint and configured with jest and react

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App.jsx';
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state ={}
-  }
 
-  render() {
-    return (
-      <h1>Hello World!</h1>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
