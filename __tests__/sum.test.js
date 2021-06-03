1ba4c9aa5a2f9290c4c670262962622947bc8b99
izzyfarley <izzyfarley33@gmail.com>
2021-06-02 18:58:12 -0700

    Test  Please enter the commit message for your changes. Lines starting

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -10,15 +10,15 @@ configure({ adapter: new Adapter() });
 import {shallow} from 'enzyme';
 
 
-describe('Testing Jest to make sure it works', () => {
-  it('adds 1 + 3 to equal 4', () => {
-    expect(sum(1,3)).toBe(4);
-  })
+// describe('Testing Jest to make sure it works', () => {
+//   it('adds 1 + 3 to equal 4', () => {
+//     expect(sum(1,3)).toBe(4);
+//   })
 
-  it('Renders Hello World', () => {
-    const app = shallow(<App />)
+//   it('Renders Hello World', () => {
+//     const app = shallow(<App />)
 
-    expect(app.exists()).toBe(true);
-  })
-})
+//     expect(app.exists()).toBe(true);
+//   })
+// })
 
