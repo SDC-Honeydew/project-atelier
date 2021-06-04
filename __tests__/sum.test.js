6d917cfaed55c6897c8c4a05284a6b660a8712c4
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-04 13:28:56 -0500

    Delete config import

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import App from '../client/src/app.jsx';
 
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
--- client/src/overview/overview.jsx
+++ client/src/overview/overview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 //import config from '../../../config.js';
-import config from '/config.js';
+//import config from '/config.js';
 
 class Overview extends React.Component {
   constructor(props) {
