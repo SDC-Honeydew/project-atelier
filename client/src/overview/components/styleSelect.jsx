889bf3472387177a0b1f57eb804e1d00f08359f9
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-07 09:44:27 -0500

    Rounded styles images

--- client/src/overview/components/styleSelect.jsx
+++ client/src/overview/components/styleSelect.jsx
@@ -8,20 +8,18 @@ class StyleSelector extends React.Component {
 
   render() {
     return (
-      <ul className='overview-styles-thumbnails' data-testid='style-select'>
+      <ul className='overview-styles-thumbnails-container' data-testid='style-select'>
         {this.props.styles.map(style => {
           return (
-
-              <li className='overview-styles-img'>
-                <img
-                  src={style.photos[0].thumbnail_url}
-
-                  height='50'
-                  width='50'>
-                </img>
-                <p>{style.name}</p>
-              </li>
-
+            <li className='overview-styles-imgs'>
+              <img
+                src={style.photos[0].thumbnail_url}
+                className='overview-styles-img'
+                height='50'
+                width='50'>
+              </img>
+              <p>{style.name}</p>
+            </li>
           );
         })}
       </ul>
