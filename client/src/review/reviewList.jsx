c77231e3a704db16471af9d30e890cab3af2e54f
minghakcer <maming1990@gmail.com>
2021-06-09 09:22:46 -0600

    chang attribute 'class' to 'className'

--- client/src/review/reviewList.jsx
+++ client/src/review/reviewList.jsx
@@ -5,10 +5,12 @@ import ReviewSort from './reviewSort.jsx';
 const ReviewList = (props) => (
   <div className='review-list'>
     <ReviewSort count={props.data.count} />
-    {props.data.results.map(result => <ReviewComp result={result} />)}
+    <div className='overflow-auto' style={{ height: '500px' }}>
+      {props.data.results.map(result => <ReviewComp result={result} />)}
+    </div>
     <div>
-      <button className='btn btn-primary mr-2' id='more-review'>MORE REVIEWS</button>
-      <button className='btn btn-primary mr-2' id='add-review'>ADD A REVIEW</button>
+      <button className='btn btn-primary m-3 ' id='more-review'>MORE REVIEWS</button>
+      <button className='btn btn-primary m-3 ' id='add-review'>ADD A REVIEW</button>
     </div>
   </div>
 );
