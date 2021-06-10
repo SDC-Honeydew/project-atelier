d3aeab8b244bf49d16282506e378322b52b6231a
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-10 13:34:34 -0500

    Function done with moveing through photos

--- client/src/overview/components/imageGallery.jsx
+++ client/src/overview/components/imageGallery.jsx
@@ -5,34 +5,42 @@ class ImageGallery extends React.Component {
     super(props);
     this.state = {
       mainImg: this.props.photos[0].url,
+      currentIndex: 0,
       thumbnailImgs: this.props.photos,
       currentImg: null,
+      firstImg: this.props.photos[0].url,
+      lastImg: this.props.photos[this.props.photos.length - 1].url
 
     };
 
     this.setMainImage = this.setMainImg.bind(this)
   }
 
-  setMainImg(e) {
+  setMainImg(e, i) {
     e.preventDefault();
-    var mainImg = e.target.src;
+    var mainImg = e.target.src || this.props.photos[i].url;
     this.setState({
-      mainImg
+      mainImg,
+      currentIndex: i
     })
   }
-//use current image to show currently selected property
-//on click to change current image
-//make img its own seperate component to pass down current image props to render
+//need state for first and last picture
+//display right arrow on fist img
+//display left arrow on last img
+//display both other wise
+
 
   render() {
     return (
       <div className='overview-image-gallery' data-testid='image-gallery'>
         <img src={this.state.mainImg} width='400' height='600'></img>
         <div className='overview-thumbnails'>
-          {this.state.thumbnailImgs.map((img, key) => (
-            <img onClick={(e) => this.setMainImg(e)} className={`overview-thumbnails-img${this.state.mainImg === img.url ? '-highlight' : ''}`}src={img.url} width='100' height='100'></img>
+          {this.state.thumbnailImgs.slice(0, 7).map((img, key) => (
+            <img onClick={(e) => this.setMainImg(e, key)} className={`overview-thumbnails-img${this.state.mainImg === img.url ? '-highlight' : ''}`}src={img.url} width='100' height='100'></img>
           ))}
         </div>
+        <div onClick={(e) => this.setMainImage(e, this.state.currentIndex - 1)}className='overview-image-gallery-left-arrow'>L</div>
+        <div onClick={(e) => this.setMainImage(e, this.state.currentIndex + 1)} className='overview-image-gallery-right-arrow'>R </div>
       </div>
     );
   }
