9cf37d882a9ddb2f36b8ded202ac8f4370b3d754
Margaret Kearns <margokearns@gmail.com>
2021-06-09 23:07:19 -0400

    Pulling data for multiple products, about to attempt refactor

--- server/controllers/related.js
+++ server/controllers/related.js
@@ -4,6 +4,7 @@ const config = require('../../config.js');
 
 module.exports = {
   getRelatedProducts: (req, res) => {
+    var relatedProductData = [];
     // get list of related products
     axios({
       method: 'get',
@@ -12,14 +13,42 @@ module.exports = {
         'Authorization': `${config.TOKEN}`
       }
     // for each product
-    }).then(response => console.log(response.data)).catch(err => console.log(err));
-      //do a request to the ID
-      //add the ID, name, category, features to an object
+    }).then((response) => {
 
-      //do a request to the style
-      //ad the original price, sale price, and photo from the default style to the object
+      relatedProductIds = response.data;
 
-      //do a request to reviews
-      //add the average rating for that item to the object
+      // relatedProductIds.forEach((Id) => {
+      //   relatedProductData.push({ id: Id});
+      // });
+
+      axios.all(relatedProductIds.map((relatedProductID) =>
+
+        axios({
+          method: 'get',
+          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${relatedProductID}`,
+          headers: {
+            'Authorization': `${config.TOKEN}`
+          }
+        // for each product
+        })
+
+      )).then(axios.spread((...responses) => {
+        responses.forEach((response) => {
+          relatedProductData.push(response.data);
+        });
+        console.log(relatedProductData);
+      })).catch((err) => {
+        console.log('err at related.js', err);
+      });
+
+    }).catch(err => console.log(err));
+    // do a request to the ID
+    // add the ID, name, category, features to an object
+
+    // do a request to the style
+    // ad the original price, sale price, and photo from the default style to the object
+
+    // do a request to reviews
+    // add the average rating for that item to the object
   }
 };
