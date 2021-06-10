429f27ae27aa88bee47a07cc603116ec31214c1b
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-10 13:06:58 -0500

    Highlight img on selection

--- client/src/overview/components/imageGallery.jsx
+++ client/src/overview/components/imageGallery.jsx
@@ -4,20 +4,20 @@ class ImageGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainPhoto: this.props.photos[0].url,
-      thumbnailPhotos: this.props.photos,
-      currentImage: null,
+      mainImg: this.props.photos[0].url,
+      thumbnailImgs: this.props.photos,
+      currentImg: null,
 
     };
 
-    this.setMainImage = this.setMainImage.bind(this)
+    this.setMainImage = this.setMainImg.bind(this)
   }
 
-  setMainImage(e) {
+  setMainImg(e) {
     e.preventDefault();
-    var mainPhoto = e.target.src;
+    var mainImg = e.target.src;
     this.setState({
-      mainPhoto
+      mainImg
     })
   }
 //use current image to show currently selected property
@@ -27,10 +27,10 @@ class ImageGallery extends React.Component {
   render() {
     return (
       <div className='overview-image-gallery' data-testid='image-gallery'>
-        <img src={this.state.mainPhoto} width='400' height='600'></img>
+        <img src={this.state.mainImg} width='400' height='600'></img>
         <div className='overview-thumbnails'>
-          {this.state.thumbnailPhotos.map((photo, key) => (
-            <img onClick={(e) => this.setMainImage(e)}className='overview-thumbnails-img'src={photo.url} width='100' height='100'></img>
+          {this.state.thumbnailImgs.map((img, key) => (
+            <img onClick={(e) => this.setMainImg(e)} className={`overview-thumbnails-img${this.state.mainImg === img.url ? '-highlight' : ''}`}src={img.url} width='100' height='100'></img>
           ))}
         </div>
       </div>
