01dcc1c7eaa8f45bccae199d97a6c9fbee528e8e
Margaret Kearns <margokearns@gmail.com>
2021-06-10 23:05:48 -0400

    WIP sale price

--- client/src/related/productCard.jsx
+++ client/src/related/productCard.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StarRating from '../review/starRating.jsx';
 
+class SalePrice extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var jsx;
+    if (this.props.sale_price === null) {
+      jsx = (
+        <p className='related_details'>${this.props.original_price}</p>
+      );
+    } else {
+      jsx = (
+        <React.Fragment>
+          <span className='related_sale'>${this.props.sale_price}</span><span>${this.props.productInfo.original_price}</span>
+        </React.Fragment>
+      );
+      return jsx;
+    }
+  }
+}
+
 class ProductCard extends React.Component {
   constructor(props) {
     super(props);
@@ -16,9 +38,9 @@ class ProductCard extends React.Component {
     return (
       <div className="related_productCard">
         <img className='related_productCardImage' src = {this.props.productInfo.image || 'https://static.thenounproject.com/png/1103191-200.png'}></img>
-        <p>{this.props.productInfo.category}</p>
-        <p>{this.props.productInfo.name}</p>
-        <p>${this.props.productInfo.original_price}</p>
+        <p className='related_details'>{this.props.productInfo.category}</p>
+        <p className='related_details'>{this.props.productInfo.name}</p>
+        <SalePrice original_price={this.props.productInfo.original_price} sale_price={this.props.productInfo.sale_price}/>
         <StarRating rating={this.props.productInfo.avgReview}/>
       </div>
     );
