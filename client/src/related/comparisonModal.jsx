108d351d33c63b570e76124bd36dfcb5cf880d7d
Margaret Kearns <margokearns@gmail.com>
2021-06-20 17:52:30 -0400

    Pulling data for products to be compared into object

--- client/src/related/comparisonModal.jsx
+++ client/src/related/comparisonModal.jsx
@@ -5,29 +5,57 @@ class ComparisonModal extends React.Component {
   }
 
   render() {
-    console.log('current product features:', this.props.currentProd);
-    console.log('comparison product features:', this.props.comparisonProd);
     if (!this.props.showModal) {
       return null;
-    }
-    return <div className='related_modal'>
-      <div onClick={(event) => {
-        event.preventDefault();
-        this.props.closeModal();
-      }}>X</div>
-      <h6>COMPARING</h6>
-      <table className='comparison_table'>
-        <thead>
-          <tr>
-            <th>Product 1</th>
-            <th></th>
-            <th>Product 2</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
+    } else {
+      var current = this.props.currentProd;
+      var comparison = this.props.comparisonProd;
+      //create an object containing all features present in either product
+      var allFeatures = {};
+      current.features.forEach((feature) => {
+        allFeatures[feature.feature] =
+        {
+          current: feature.value
+        };
+      });
 
-    </div>;
+      comparison.features.forEach((feature) => {
+        if (allFeatures[feature.feature]) {
+          allFeatures[feature.feature].comparison = feature.value;
+        } else {
+          allFeatures[feature.feature] =
+          {
+            comparison: feature.value
+          };
+        }
+      });
+
+      console.log('/////features to display,');
+      console.log(allFeatures);
+
+      return (<div className='related_modal'>
+        <div onClick={(event) => {
+          event.preventDefault();
+          this.props.closeModal();
+        }}>X</div>
+        <h6>COMPARING</h6>
+        <table className='comparison_table'>
+          <thead>
+            <tr>
+              <th>{current.name}</th>
+              <th></th>
+              <th>{comparison.name}</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+
+      </div>);
+
+    }
+
 
 
   }
