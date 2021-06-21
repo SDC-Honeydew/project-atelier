b20ec5bfc35c04573cc9803e10c054cb99cc6c3a
Margaret Kearns <margokearns@gmail.com>
2021-06-20 23:54:58 -0400

    Comparison Modal Functional

--- client/src/related/comparisonModal.jsx
+++ client/src/related/comparisonModal.jsx
@@ -15,6 +15,7 @@ class ComparisonModal extends React.Component {
       current.features.forEach((feature) => {
         allFeatures[feature.feature] =
         {
+          feature: feature.feature,
           current: feature.value
         };
       });
@@ -25,6 +26,7 @@ class ComparisonModal extends React.Component {
         } else {
           allFeatures[feature.feature] =
           {
+            feature: feature.feature,
             comparison: feature.value
           };
         }
@@ -33,22 +35,37 @@ class ComparisonModal extends React.Component {
       console.log('/////features to display,');
       console.log(allFeatures);
 
+      var featureRows = [];
+
+      for (var key in allFeatures) {
+        featureRows.push(
+          <tr>
+            <td className='related_comparisonCurrent'>{allFeatures[key].current || null}</td>
+            <td className='related_comparisonFeature'>{allFeatures[key].feature}</td>
+            <td className='related_comparisonCompare'>{allFeatures[key].comparison || null}</td>
+          </tr>
+        );
+      }
+
+      // console.log('//// FEATURE ROWS OBJECT');
+      // console.log(featureRows);
+
       return (<div className='related_modal'>
-        <div onClick={(event) => {
+        <div className='related_closeModal' onClick={(event) => {
           event.preventDefault();
           this.props.closeModal();
         }}>X</div>
-        <h6>COMPARING</h6>
-        <table className='comparison_table'>
+        <p>COMPARING</p>
+        <table className='related_comparisonTable'>
           <thead>
             <tr>
               <th>{current.name}</th>
               <th></th>
-              <th>{comparison.name}</th>
+              <th className='related_comparisonCompare'>{comparison.name}</th>
             </tr>
           </thead>
           <tbody>
-
+            {featureRows}
           </tbody>
         </table>
 
