17e4cb24ce84bec1b36f2b395a8f6ba8082d7a62
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-23 21:49:33 -0500

    Refactor styleSelector away from class

--- client/src/overview/components/styleSelect.jsx
+++ client/src/overview/components/styleSelect.jsx
@@ -1,44 +1,27 @@
 import React from 'react';
 import ProductStyle from './productStyle.jsx';
 
-
-class StyleSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-    this.onStyleClick = this.onStyleClick.bind(this);
-  }
-
-  onStyleClick(style) {
-    this.setState({
-      currentStyle: style
-    });
-  }
-
-  render() {
-    return (
-      <div>
+const StyleSelector = (props) => {
+  return (
+    <div>
         <div className='overview-styleSelector-style'>
-          <p className='overview-style'>Style ></p><p className='overview-style-title'> {   this.props.styles[this.props.currentStyleIndex].name}</p>
+          <p className='overview-style'>Style ></p><p className='overview-style-title'> {props.styles[props.currentStyleIndex].name}</p>
         </div>
         <ul className='overview-styles-thumbnails-container' data-testid='style-select'>
-          {this.props.styles.map((style, i) => {
+          {props.styles.map((style, i) => {
             return (
               <ProductStyle
                 key={i}
                 i={i}
                 style={style}
-                toggle={this.props.setCurrentStyleIndex}
-                currentStyle={this.props.styles[this.props.currentStyleIndex].name}
-                showCheck={this.props.styles[this.props.currentStyleIndex].name === style.name}
+                toggle={props.setCurrentStyleIndex}
+                currentStyle={props.styles[props.currentStyleIndex].name}
+                showCheck={props.styles[props.currentStyleIndex].name === style.name}
               />);
           })}
         </ul>
       </div>
-    );
-  }
+  )
 }
 
 export default StyleSelector;
