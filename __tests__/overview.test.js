2e11961501f354ef74ac6ac3b99f274435526a82
Tyler Brinkman <tbrinkman3@gmail.com>
2021-06-26 16:13:51 -0500

    Remove unused import

--- __tests__/overview.test.js
+++ __tests__/overview.test.js
@@ -1,7 +1,6 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import renderer from 'react-test-renderer';
 
 import ProductOverview from '../client/src/overview/overview.jsx';
 import StyleSelector from '../client/src/overview/components/styleSelect.jsx';
